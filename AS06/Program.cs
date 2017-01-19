e8aa4752e6a19184ae5e1a0300dfe7f85d7225fd
K1493 <K1493@student.jamk.fi>
2017-01-19 10:21:16 +0200

    aaa

---
+++ AS06/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int matka;
+            int bensa = ();
+            int raha = (matka );
+
+            Console.Write("Anna matka: ");
+            Console.In()
+        }
+    }
+}
