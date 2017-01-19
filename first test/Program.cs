4b94fd4b3ad54170afa2a5b6c1f57be37c220625
K1493 <K1493@student.jamk.fi>
2017-01-19 09:25:54 +0200

    corrected salute

--- first test/Program.cs
+++ first test/Program.cs
@@ -10,7 +10,7 @@ namespace first_test
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello World.");
+            Console.Write("Hello World 2.");
         }
     }
 }
