845685d10f01c9b357bc84256034dd685f53a61e
K1493 <K1493@student.jamk.fi>
2017-01-19 10:09:10 +0200

    a

---
+++ AS5_v2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS5_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sekunnit = 3661;
+
+            int tunnit = sekunnit / 3600;
+            int min = sekunnit / 60 - tunnit * 60;
+            int sek = sekunnit % 60;
+
+            Console.Write(tunnit, min, sek);
+
+        }
+    }
+}
