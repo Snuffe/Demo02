5a85a99ec4623ceb73e2ae7f5dcc30f995461fd4
K1493 <K1493@student.jamk.fi>
2017-01-19 11:05:06 +0200

    Tunnilla tehdyt

---
+++ AS07/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Anna vuosiluku > ");
+            int vuosi = int.Parse(Console.ReadLine());
+
+            if (vuosi % 4 == 0 || vuosi % 400 == 0) {
+                Console.Write("Vuosi on karkausvuosi");
+            }
+
+            else {
+                Console.Write("Vuosi ei ole karkausvuosi \n");
+            }
+
+            
+
+
+
+        }
+    }
+}
---
+++ AS08/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Anna kokonaisnumero1 > ");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Anna kokonaisnumero2 > ");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Anna kokonaisnumero3 > ");
+            int numero3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Math.Max(Math.Max(numero1, numero2), numero3));
+
+        }
+    }
+}
