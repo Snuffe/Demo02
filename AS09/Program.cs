9103c201abdf7cb109597fbbf2b0f9fd9eb54803
K1493 <K1493@student.jamk.fi>
2017-01-20 13:23:36 +0200

    teht 9

---
+++ AS09/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int summa = 0;
+            int n = 1;
+
+            Console.Write("Anna kokonaisnumero " + n + " > ");
+            int numero = int.Parse(Console.ReadLine());
+
+            while (numero != 0)
+            {
+                n++;
+                summa = (summa + numero);
+                Console.Write("Anna kokonaisnumero " + n + " > ");
+                numero = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.Write("summa: " + summa + "\n");
+
+
+
+
+
+        }
+
+                }
+
+        }
