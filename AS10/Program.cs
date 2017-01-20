f8ea1902c60f786bdac11d76aa4dc139d718128d
K1493 <K1493@student.jamk.fi>
2017-01-20 13:46:33 +0200

    teht10

--- AS10/Program.cs
+++ AS10/Program.cs
@@ -10,7 +10,24 @@ namespace AS10
     {
         static void Main(string[] args)
         {
+            int [] luvut = { 1, 2, 33, 44, 55, 68, 77, 96, 100 };
+            int n = 0;
+            while (n < luvut.Length){
+                if(n % 2 == 0)
+                {
+                    Console.Write("HEP \n");
+                    n++;
+                    continue;
+                }
+                else
+                {
+                    n++;
+                    continue;
+                }
 
+            }
+
+            }
 
 
         }
@@ -18,4 +35,3 @@ namespace AS10
 
         
     }
-}
