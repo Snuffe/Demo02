bb1c07865ff5364831e32256b4f65e50837d1a8e
K1493 <K1493@student.jamk.fi>
2017-01-20 13:57:58 +0200

    teht11 kesken

---
+++ AS11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna tahtirivien lukumaara: ");
+            int n = int.Parse(Console.ReadLine());
+            int i = 1;
+            for (i in n)
+            {
+                Console.Write("* \n");
+            }
+
+        }
+    }
+}
